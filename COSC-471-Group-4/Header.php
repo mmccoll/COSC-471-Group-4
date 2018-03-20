398b9b0b5971d818bfe5a28416854931bd2fd08a
mmccoll <35944088+mmccoll@users.noreply.github.com>
2018-03-20 16:39:01 -0400

    test2
    testing

---
+++ COSC-471-Group-4/Header.php
@@ -0,0 +1,23 @@
+<?php
+echo '<p>this is my header</p>';
+?>
+<!--<html>
+    <style>
+        header, td
+        {
+            text-align: center;
+            background-color: darkcyan;
+            color: white;
+        }
+    </style>
+	<header>
+        <table align="center">
+            <tr>
+                <td><a>Home&nbsp;&nbsp;</a></td>
+                <td><a>Inventory&nbsp;&nbsp;</a></td>
+                <td><a>Cart&nbsp;&nbsp;</a></td>
+                <td><a>Account&nbsp;&nbsp;</a></td>
+            </tr>
+        </table>
+	</header>
+</html>-->
