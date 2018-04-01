cf3482de10d79db2118bdec26d9061156dbac754
Josh <Josh@Josh-PC>
2018-04-01 18:29:59 -0400

    -Admin.php: added links to navigate to AdminAddTo.php -AdminAddTo.php: added AdminAddTo.php page for adding items to tables. -processSQL.php: added processSQL.php which uses SQL to add item to item table and also the specific items table. -Home.php: added ability for admin accounts to navigate to admin.php instead of the home.php. -Index.php: changed line 20: it said Is_admin='1', changed to Is_admin='true'.

---
+++ COSC-471-Group-4/Index.php
@@ -0,0 +1,73 @@
+<?php
+
+// restart the session
+session_abort();
+session_start();
+$_SESSION["fromLogin"] = true;
+$_SESSION["loggedIn"] = false;
+
+// check if admin account exists in db
+// if not, insert an admin account
+
+$user = 'root';
+$password = '';
+$db = 'BOOKSTORE';
+//connects to database
+$dbs = new mysqli('localhost', $user, $password, $db) or die("Unable to connect with db.");
+
+// USERS(Name, Address, Email, Username, Password, Is_Admin)
+
+$sql = "SELECT * FROM USERS WHERE Is_Admin='true'";
+
+$result = $dbs->query($sql);
+if ($result->num_rows == 0)
+{
+    $sql = "INSERT INTO USERS (Name, Address, Email, Username, Password, Is_Admin) VALUES ";
+    $sql .= "('Admin', '', 'admin471@gmail.com', 'admin', 'admin123', 'true');";
+    
+    if ($dbs->query($sql) !== TRUE) 
+    {
+        echo "Error: " . $sql . "<br>" . $dbs->error;
+    } 
+}
+
+
+?>
+<html>
+<title>The Office Supply Store</title>
+<body>
+    <h2 align="center">Log In</h2>
+    <form action="Home.php" method="post">
+        <table align="center">
+        <tr>
+            <td>Username:</td>
+            <td><input required="required" type="text" name="username"/></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input required="required" type="password" name="password"/></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Log In"></button></td>
+        </tr>
+    </table>
+    </form>
+    <table align="center">
+        <tr>
+            <td>
+                <a href='Register.php'>
+                    <p style="text-align:center">Don't have an account? Create one!</p>
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href='ForgotPassword.php'>
+            <p style="text-align:center">Forgot Password?</p>
+        </a>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
+
