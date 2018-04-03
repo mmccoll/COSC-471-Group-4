d6b016989633107f8cd11ef36658231e4268c8c0
mmccoll <35944088+mmccoll@users.noreply.github.com>
2018-04-03 17:14:39 -0400

    Piecing pages together
    - Header.php: adding hrefs to the respective pages
    - Cart.php: creating a placeholder until Dhwani pushes her code
    - Home.php: if anything changed, not sure what it is

--- COSC-471-Group-4/Header.php
+++ COSC-471-Group-4/Header.php
@@ -19,56 +19,17 @@ $php = '<html>
     </style>
 	<header>
         <div id="header">
-            <div>
-                <div><img src="Images\Logo.png"></img>
-            </div>
-            <div>
+            <div><img src="Images\Logo.png"></img>
+            <div align="right"><a href="Jingjing Zhou/UserProfile.html">hello ' . $username;
+$php .= '</a></div> 
             <table align="center" id="links">
                 <tr>
-                    <td><a href="Home.php">Home&nbsp;&nbsp;</a></td>
-                    <td><a>Cart&nbsp;&nbsp;</a></td>
-                    <td><a>Account&nbsp;&nbsp;</a></td>
+                    <td><a href="Home.php">Home</a>&nbsp;&nbsp;</td>
+                    <td><a href="Cart.php">Cart</a>&nbsp;&nbsp;</td>
                 </tr>
             </table>
-            <p>hello</p>
-            </div>
         </div>
 	</header>
 </html>';
-
-/*$php = '<html>
-    <style>
-        #header, #links
-        {
-            background-color: darkcyan;
-            color: white;
-            font-size: xx-large;
-        }
-        img
-        {
-            align: left;
-            width: 100px;
-            height: 100px;
-            padding: 5px;
-        }
-    </style>
-	<header>
-        <div id="header">
-            <div><img src="Images\Logo.png"></img>
-            <div align="right"><p>hello ' . $username;
-$php .= '</p></div> 
-            <table align="center" id="links">
-                <tr>
-                    <td><a href="Home.php">Home&nbsp;&nbsp;</a></td>
-                    <td><a>Cart&nbsp;&nbsp;</a></td>
-                    <td><a>Account&nbsp;&nbsp;</a></td>
-                </tr>
-            </table>
-        </div>
-	</header>
-</html>';*/
 echo $php;
 ?>
-<style>
-    
-</style>
