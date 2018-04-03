0b16472613f641e326f54b4eb9ca6f78eba411cc
mmccoll <35944088+mmccoll@users.noreply.github.com>
2018-04-03 16:23:00 -0400

    - starting the sign out html

--- COSC-471-Group-4/Header.php
+++ COSC-471-Group-4/Header.php
@@ -1,5 +1,7 @@
 <?php
-echo '<html>
+$username = $_SESSION["username"];
+
+$php = '<html>
     <style>
         #header, #links
         {
@@ -17,15 +19,56 @@ echo '<html>
     </style>
 	<header>
         <div id="header">
-        <img src="Images\Logo.png"></img>
-        <table align="center" id="links">
-            <tr>
-                <td><a href="Home.php">Home&nbsp;&nbsp;</a></td>
-                <td><a>Cart&nbsp;&nbsp;</a></td>
-                <td><a>Account&nbsp;&nbsp;</a></td>
-            </tr>
-        </table>
+            <div>
+                <div><img src="Images\Logo.png"></img>
+            </div>
+            <div>
+            <table align="center" id="links">
+                <tr>
+                    <td><a href="Home.php">Home&nbsp;&nbsp;</a></td>
+                    <td><a>Cart&nbsp;&nbsp;</a></td>
+                    <td><a>Account&nbsp;&nbsp;</a></td>
+                </tr>
+            </table>
+            <p>hello</p>
+            </div>
         </div>
 	</header>
 </html>';
+
+/*$php = '<html>
+    <style>
+        #header, #links
+        {
+            background-color: darkcyan;
+            color: white;
+            font-size: xx-large;
+        }
+        img
+        {
+            align: left;
+            width: 100px;
+            height: 100px;
+            padding: 5px;
+        }
+    </style>
+	<header>
+        <div id="header">
+            <div><img src="Images\Logo.png"></img>
+            <div align="right"><p>hello ' . $username;
+$php .= '</p></div> 
+            <table align="center" id="links">
+                <tr>
+                    <td><a href="Home.php">Home&nbsp;&nbsp;</a></td>
+                    <td><a>Cart&nbsp;&nbsp;</a></td>
+                    <td><a>Account&nbsp;&nbsp;</a></td>
+                </tr>
+            </table>
+        </div>
+	</header>
+</html>';*/
+echo $php;
 ?>
+<style>
+    
+</style>
